1c5d1f3540defeb8f61d6f5ed7cb52348b9a74db
sssungjooon <dellojoon7@gmail.com>
2024-10-10 17:15:57 +0900

    Java 241010 upload

---
+++ Java/SCIT/javaSE/src/chapter00/basic/C001_info.java
@@ -0,0 +1,34 @@
+package chapter00.basic;
+
+public class C001_info {
+
+	public static void main(String[] args) {
+		// 한줄 주석
+		/*
+		 * 여러 줄 주석
+		 * java 프로그램을 실행하기 위해서는 main 메서드가 필요 
+		 * 
+		 */
+		/**
+		 * 색깔이 다른 여러줄 주석
+		 * 클래스, 메서드, 코드를 설명하기 위한 주석
+		 */
+		// 출력을 도와주는 메서드 
+		System.out.println("hello world");
+
+		// 코드 저장 => ctrl + s
+		// 출력, 실행 => ctrl + f11
+		/*
+		 * ctrl + space : 입력 중에 완성 기능 호출 
+		 * ctrl + d : 한줄 삭제
+		 * ctrl + shift + f : 코드 자동 정렬
+		 * alt + 방향키(↑↓) : 위 (아래) 줄로 이동
+		 * ctrl + alt + up(down) : 한 줄, 선택 블럭 복사
+		 * ctrl + shift + O : 자동 import
+		 * ctrl + shift + s : 모든 소스코드 저장 
+		 * ctrl + / : 주석 자동 완성
+		 * ctrl + shift + / : 여러 줄 주석 자동 완성 
+		 */
+	}
+
+}
