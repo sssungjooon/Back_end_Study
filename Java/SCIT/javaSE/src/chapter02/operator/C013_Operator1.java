3e4708a44e4903aecce7d8eb894853ffe750c2c4
sssungjooon <dellojoon7@gmail.com>
2024-10-15 19:36:45 +0900

    241015_Java_upload

---
+++ Java/SCIT/javaSE/src/chapter02/operator/C013_Operator1.java
@@ -0,0 +1,20 @@
+package chapter02.operator;
+
+public class C013_Operator1 {
+
+	public static void main(String[] args) {
+		// 산술 연산자
+		int a = 6, b = 4;
+		System.out.println(a + " + " + b +  " = " + (a+b));
+		System.out.println(a + " - " + b +  " = " + (a-b));
+		System.out.println(a + " * " + b +  " = " + (a*b));
+		System.out.println(a + " / " + b +  " = " + (a/b));
+		System.out.println(a + " / " + b +  " = " + ((float) a/b));
+		
+		// % 연산자 
+		System.out.println("6 % 4 = " + 6 % 4);
+		System.out.println("7 % 3 = " + 7 % 3);
+		System.out.println("15 % 2 = " + 15 % 2);
+	}
+
+}
